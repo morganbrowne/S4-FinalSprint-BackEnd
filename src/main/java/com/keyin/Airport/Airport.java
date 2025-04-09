f5fdee708ca9a5c1701576282bea09f1a24da230
Morgan Browne <morgan.browne@keyin.com>
2025-04-09 13:25:45 -0230

    Cloned Previous back end repo from first sprint

---
+++ src/main/java/com/keyin/Airport/Airport.java
@@ -0,0 +1,55 @@
+package com.keyin.Airport;
+
+import java.lang.annotation.Inherited;
+
+import javax.annotation.processing.Generated;
+
+import com.keyin.City.City;
+import jakarta.persistence.*;
+
+@Entity
+public class Airport {
+    @Id
+    @SequenceGenerator(name = "airport_sequence", sequenceName = "airport_sequence", allocationSize = 1, initialValue = 1)
+    @GeneratedValue(generator = "airport_sequence")
+    private long id;
+
+    private String name;
+    private String code;
+
+    @ManyToOne
+    @JoinColumn(name = "city_id")
+    private City city;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public City getCity() {
+        return city;
+    }
+
+    public void setCity(City city) {
+        this.city = city;
+    }
+}
---
+++ src/main/java/com/keyin/City/City.java
@@ -0,0 +1,46 @@
+package com.keyin.City;
+
+import com.keyin.Airport.Airport;
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+public class City {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String province;
+    private int population;
+
+    @OneToMany(mappedBy = "city", cascade = CascadeType.ALL)
+    private List<Airport> airports;
+
+
+    public long getId() {
+        return id;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+
+}
